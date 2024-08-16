d3ceeb06aa46fe1f89c4d01cca97af2932bd1736
gneureuther <george.neureuther@gmail.com>
2024-08-16 14:56:06 -0400

    udpated server.py

--- server.py
+++ server.py
@@ -4,9 +4,6 @@ Running this file will start a flask server on port 5000
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-# Response message
-MESSAGE = "For the given statement, the system response is "
-
 # Create the flask app
 app = Flask("Emotion Analyser")
 
@@ -19,9 +16,21 @@ def render_index_page():
 
 @app.route('/emotionDetector')
 def emotion_analyzer():
+    # Response message
+    message = "For the given statement, the system response is "
+    # get the text entered
     text_to_analyze = request.args.get('textToAnalyze')
+    # Analyze the text
     response = emotion_detector(text_to_analyze)
-    return {MESSAGE: response}    
+    # Build the final message to be displayed
+    #message += response + dominant_str
+    message += "'anger':" + str(response['anger']) + ', '
+    message += "'disgust':" + str(response['disgust']) + ', '
+    message += "'fear':" + str(response['fear']) + ', '
+    message += "'joy':" + str(response['joy']) + ', '
+    message += "'sadness':" + str(response['sadness']) + '. '
+    message += "The dominant emotion is " + response['dominant_emotion']
+    return message    
 
 # Run the flask app on localhost port 5000
 if __name__ == "__main__":
